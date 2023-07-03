6ea8d88640824a9c912f403035023fee745e091d
me.tinchost <me.tinchost@gmail.com>
2023-07-03 02:09:16 -0300

    MEAN fronend

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
-const routes: Routes = [];
+const routes: Routes = [ //aca defino las rutas
+  {path:  '',component: ListaProductosComponent},
+  {path:'crear-producto', component: CrearProductoComponent},
+  {path:'editar-producto/:id',component: CrearProductoComponent},
+  {path:'**',redirectTo:'',pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms'; // se importa la libreria de ReactiveFormsModule
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+//COMPONENTES
 import { AppComponent } from './app.component';
+import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CrearProductoComponent,
+    ListaProductosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule // para trabajar con formularios
   ],
   providers: [],
   bootstrap: [AppComponent]
