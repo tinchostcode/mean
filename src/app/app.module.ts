755582109a0aa8e9fa27330458d6961272df9f64
me.tinchost <me.tinchost@gmail.com>
2023-07-04 16:08:01 -0300

    Frontend terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms'; // se importa la libreria de ReactiveFormsModule
 import { BrowserModule } from '@angular/platform-browser';
-
+ 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
-//COMPONENTES
+  //COMPONENTES
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
 import { ListaProductosComponent } from './components/lista-productos/lista-productos.component';
+
 
 @NgModule({
   declarations: [
@@ -18,7 +21,10 @@ import { ListaProductosComponent } from './components/lista-productos/lista-prod
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule // para trabajar con formularios
+    ReactiveFormsModule, // para trabajar con formularios
+    BrowserAnimationsModule, // toastr
+    ToastrModule.forRoot()
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/crear-producto/crear-producto.component.ts
+++ src/app/components/crear-producto/crear-producto.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+
 import { FormGroup, FormBuilder,Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Producto } from 'src/app/models/producto';
 
 @Component({
   selector: 'app-crear-producto',
@@ -9,9 +13,13 @@ import { FormGroup, FormBuilder,Validators } from '@angular/forms';
 export class CrearProductoComponent implements OnInit {
 
   productoForm: FormGroup; //tengo que inicializarlo en el constructor
+  
 
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private toastr:ToastrService) {
 
-  constructor(private fb: FormBuilder) {
     this.productoForm=this.fb.group({
       producto:['',Validators.required],
       categoria:['',Validators.required],
@@ -21,8 +29,24 @@ export class CrearProductoComponent implements OnInit {
    }
 
   ngOnInit(): void {
+   
   }
-agregarProducto(){
-  console.log(this.productoForm);
-}
+  
+  agregarProducto(){
+    //console.log(this.productoForm);
+
+    const PRODUCTO: Producto = { // VER LUEGO
+
+      nombre: this.productoForm.get('producto')?.value,
+      categoria: this.productoForm.get('categoria')?.value,
+      ubicacion: this.productoForm.get('ubicacion')?.value,
+      precio: this.productoForm.get('precio')?.value,
+      
+    }
+    console.log(PRODUCTO);
+    this.router.navigate(['/'])
+    this.toastr.success('EXITO!')
+    //this.toastr.error('EXITO!')
+    //this.toastr.info('EXITO!')
+  }
 }
